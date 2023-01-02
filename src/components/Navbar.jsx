5b28d0ddec14b1510cde01689092e8ac76a06cfd
olawale <jao.olaniyi@gmail.com>
2023-01-02 11:08:56 +0100

    navbar scroll effect

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BiShoppingBag } from "react-icons/bi";
 
-export default function navbar() {
+export default function Navbar() {
+  const [scroll, setScroll] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setScroll(window.scrollY > 20);
+    });
+  }, []);
   return (
-    <div className="fixed top-0 left-0 w-full z-20">
+    <div
+      className={`${
+        scroll ? "bg-sky-500 shadow-lg" : ""
+      } fixed top-0 left-0 w-full z-20`}
+    >
       <div className=" flex items-center justify-between relative container  py-4 px-2 mx-auto">
         <div className="font-bold text-xl">Shopify</div>
         <div className="relative cursor-pointer">
--- src/components/ShoppingItems.jsx
+++ src/components/ShoppingItems.jsx
@@ -4,9 +4,15 @@ export default function ShoppingItems({ item }) {
   const { id, image, name, price } = item;
   return (
     <div>
-      <div className="flex items-center justify-center h-[400px] bg-indigo-500 ">
+      <div className="flex items-center justify-center h-[400px] bg-sky-500/50 ">
         <img src={image} alt="" className="w-[200px]" />
-        <p>{price}</p>
+      </div>
+      <div className="mt-6 flex justify-between items-center px-4">
+        <div>
+          <div className="text-sm font-bold mb-3"> {name}</div>
+          <div className="text-xl font-bold">${price}</div>
+        </div>
+        <button className="bg-sky-500 p-3 rounded-500">Add to Cart</button>
       </div>
     </div>
   );
