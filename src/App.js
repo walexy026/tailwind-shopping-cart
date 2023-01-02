79f50df2c780ea9fcc64d73ec98906c3b7fdfb0f
olawale <jao.olaniyi@gmail.com>
2023-01-02 09:06:31 +0100

    added assets

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">hello world</h1>
+    </div>
+  );
 }
 
 export default App;
