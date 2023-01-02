db10e9da6795c4b6d6e67b89bc091139dfac85e5
olawale <jao.olaniyi@gmail.com>
2023-01-02 10:40:43 +0100

    added shopping items bg

--- src/components/ShoppingContainer.jsx
+++ src/components/ShoppingContainer.jsx
@@ -4,7 +4,7 @@ import ShoppingItems from "./ShoppingItems";
 
 export default function ShoppingContainer() {
   return (
-    <div className="section grid lg:grid-cols-3 md:grid-cols-2 gap-6">
+    <div className="section grid lg:grid-cols-3 md:grid-cols-2 gap-6 ">
       {items.map((item) => {
         return <ShoppingItems key={item.id} item={item} />;
       })}
--- src/components/ShoppingItems.jsx
+++ src/components/ShoppingItems.jsx
@@ -4,7 +4,7 @@ export default function ShoppingItems({ item }) {
   const { id, image, name, price } = item;
   return (
     <div>
-      <div className="flex items-center justify-center h-[400px] bg-blue rounded-full">
+      <div className="flex items-center justify-center h-[400px] bg-indigo-500 ">
         <img src={image} alt="" className="w-[200px]" />
         <p>{price}</p>
       </div>
