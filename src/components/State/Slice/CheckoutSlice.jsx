79aeb7193a0d668e2ac51b00ea796684462f58a8
olawale <jao.olaniyi@gmail.com>
2023-01-03 12:01:59 +0100

    adding some redux params

---
+++ src/components/State/Slice/CheckoutSlice.jsx
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    isOpen:false
+}
+
+const CheckoutSlice =() =>{
+    name: 'checkout',
+    initialState,
+    reducers: {}
+}
+
+export default CheckoutSlice.reducers
